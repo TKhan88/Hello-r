a8fd9256edb49686727b7a5b58438af7fcd89cab
TKhan88 <engrtariq120@gmail.com>
2021-12-12 22:24:30 +0500

    few lines added

--- testing.r
+++ testing.r
@@ -1,3 +1,9 @@
 # This is from Git.
 print("Hello from Github.")
 
+name = "Tariq Khan"
+project = "Writting a new code in Rstudio"
+version = "This is Rstudio new project of GitHub"
+print(name)
+print(project)
+print(version)
